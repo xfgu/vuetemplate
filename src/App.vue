6635b82ce8041507a284910d03aa4351445d0150
lichao <632167902@qq.com>
2020-09-11 17:02:11 +0800

    引入路由

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
   <div id="app">
     <el-container class="h-100 d-flex flex-d">
-      <el-header class="d-flex ai-center bb bs">Header</el-header>
+      <el-header class="d-flex ai-center bb bs">
+        <h1>Header</h1>
+        <el-menu :default-active="$route.path" mode="horizontal" router>
+          <el-menu-item index="/base" class="pl-70">
+            <span class="text-black2 tit">等值线绘制-简化</span>
+          </el-menu-item>
+          <el-menu-item index="/high" class="pl-70">
+            <span class="text-black2 tit">等值线绘制-高级</span>
+          </el-menu-item>
+        </el-menu>
+      </el-header>
       <el-container class="flex-1">
         <el-aside class="h-100" width="200px">
           <el-scrollbar class="h-100">
