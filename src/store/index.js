cfeb1fda1894b3b312ded9306cfde24fa88b875e
lichao <632167902@qq.com>
2020-12-01 15:52:11 +0800

    安装vuex-persistedstate

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from "vuex-persistedstate";
 import {
   getters
 } from './getters'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  plugins: [createPersistedState()],
   state: {
     userInfo: {}
   },
