077aee8d4dd2d1ab408e3d61c6d12077e309542a
lichao <632167902@qq.com>
2021-01-12 11:40:35 +0800

    修改w-100

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import './plugins/axios'
-import App from './App.vue'
+
 import router from './router'
 import store from './store'
 import mstore from 'store2'
@@ -9,11 +9,16 @@ import './assets/css/reset.css'
 import { config } from './assets/config.js'
 import leaflet from 'leaflet'
 import './plugins/element.js'
+import UmyUi from 'umy-ui'
+import 'umy-ui/lib/theme-chalk/index.css';// 引入样式
 Vue.config.productionTip = false
 
+import App from './App.vue'
 Vue.prototype.$mstore = mstore
 Vue.prototype.L = leaflet
 Vue.prototype.$config = config
+
+Vue.use(UmyUi);
 new Vue({
   router,
   store,
